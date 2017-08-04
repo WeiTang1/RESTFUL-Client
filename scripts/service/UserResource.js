a202ba4781264b06392a37b9c9b239b805d74f1a
Wei Tang <donatshanghai@gmail.com>
2017-08-04 11:05:06 -0400

    no message

--- scripts/service/UserResource.js
+++ scripts/service/UserResource.js
@@ -1,8 +1,8 @@
 var service = function($resource){
-    return $resource('http://ec2-52-14-188-104.us-east-2.compute.amazonaws.com/User/:firstName',{}
+    return $resource('http://ec2-52-14-188-104.us-east-2.compute.amazonaws.com:3000/User/:firstName',{}
     ,{
         'update': { method:'PUT',
-        url:'http://ec2-52-14-188-104.us-east-2.compute.amazonaws.com/User/:id',
+        url:'http://ec2-52-14-188-104.us-east-2.compute.amazonaws.com:3000/User/:id',
         params:{id:'@id'}
         }
 
