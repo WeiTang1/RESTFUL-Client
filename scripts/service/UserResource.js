ddd6b56c8b53611aa9d3fb1e3a492acc04d5cea5
Wei Tang <donatshanghai@gmail.com>
2017-08-11 09:54:15 -0400

    fixed

--- scripts/service/UserResource.js
+++ scripts/service/UserResource.js
@@ -1,5 +1,5 @@
 var service = function($resource){
-    return $resource('http://ec2-52-14-188-104.us-east-2.compute.amazonaws.com:3000/User/:firstName',{}
+    return $resource('http://ec2-18-220-191-94.us-east-2.compute.amazonaws.com:3000/User/:firstName',{}
     ,{
         'update': { method:'PUT',
         url:'http://ec2-18-220-191-94.us-east-2.compute.amazonaws.com:3000/User/:id',
